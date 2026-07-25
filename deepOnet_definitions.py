ed7fba841bd0fe465c9407a46c18e5bde21e6c06
Filippo Sottovia <sottoviafilippo@gmail.com>
2026-07-25 16:58:08 +0200

    work in progress

--- deepOnet_definitions.py
+++ deepOnet_definitions.py
@@ -1,7 +1,10 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
 from typing import Callable
 
+
+# note: many comments are pedagogical and pretty basic: written for myself while writing this class, for learning
 
 class deepOnet_Poisson(nn.Module):
     """Defines and sets up a deepOnet, trained on solutions of the 2d Poisson equation"""
@@ -36,7 +39,49 @@ class deepOnet_Poisson(nn.Module):
             nn.Linear(self.N_nodes_trunk, self.latent_dimension)
         )
 
-        self.optimizer_branch = optim.Adam(self.branch_network.parameters(), lr=0.01)
-        self.optimizer_trunk  = optim.Adam(self.trunk_network.parameters(), lr=0.01)
+        # If I had not subclassed nn.Module .parameters() would not be defined
+        self.optimizer = optim.Adam(self.parameters(), lr=0.01)
+        self.criterion = nn.MSELoss()
 
+
+    def forward(self, branch_input, trunk_input):
+        """output of the model: scalar product between the outputs of the trunk and branch network"""
+
+        branch_output = self.branch_network(branch_input)   
+        trunk_output  = self.trunk_network(trunk_input)      
+        # outer product over the two batch dims, contracted on latent dimension
+
+        return torch.einsum("bl,tl->bt", branch_output, trunk_output)
+
+
+    def fit(self, num_epochs:int, branch_X, trunk_X, Y, print_progress = True):
+        # X, Y in the training dataset. 
+        
+        self.epochs = []
+        self.losses = []
+
+        for epoch in range(num_epochs):
+    
+            predictions = self(branch_X, trunk_X)  # __call__ "does some bookkeeping" and calls the previously defined self.forward()
+            loss = self.criterion(predictions, Y)
+
+            self.optimizer.zero_grad() # otherwise the gradients would be added to the previously computed ones
+            loss.backward() # computes the gradient of the loss compute on the neural network, via model(X)
+            self.optimizer.step() # updates the weights
+
+            # Track the loss history
+            self.epochs.append(epoch + 1)
+            self. losses.append(loss.item())
+
+            if print_progress and (epoch + 1) % 100 == 0:
+                print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+
+    def fit_from_npz(self, num_epochs:int, print_progress = True):
+
+        """Fits the model using a training dataset stored in a .npz file"""
+        #TO DO: Write this function taking into account the way the file was saved
         pass
+
+
+# TO DO: IN THE FIT FUNCTION, CHECK THAT THE dimension of the array representing the function actually corresponds to the function discretization size
